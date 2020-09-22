44f82247be480bbd775e7bc44ccad22c634b4fbc
palyvodaBoi <xpalyvodax@gmail.com>
2020-09-22 10:59:55 +0300

    Updates

---
+++ precache-manifest.ff86aa52bc9ad96406b235c01010431d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7c4ad326e944e439e76eb4c72bcb9d",
+    "url": "/quiz-test-app/index.html"
+  },
+  {
+    "revision": "722a62956a8d6decf2ac",
+    "url": "/quiz-test-app/static/css/main.36e70194.chunk.css"
+  },
+  {
+    "revision": "69d5337b5d646f9e67b6",
+    "url": "/quiz-test-app/static/js/2.25d39239.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-test-app/static/js/2.25d39239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722a62956a8d6decf2ac",
+    "url": "/quiz-test-app/static/js/main.6777471d.chunk.js"
+  },
+  {
+    "revision": "4a8e9c51af8344975c5a",
+    "url": "/quiz-test-app/static/js/runtime-main.d9467d91.js"
+  },
+  {
+    "revision": "98bcd783b8bf052dbf028cd61018a6b1",
+    "url": "/quiz-test-app/static/media/arrow-left.98bcd783.svg"
+  },
+  {
+    "revision": "c1bbccee65e4ce9abd3c674154759307",
+    "url": "/quiz-test-app/static/media/arrow-right.c1bbccee.svg"
+  }
+]);
